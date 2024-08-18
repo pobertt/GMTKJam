2b4fd1beb8fbccdca3927f4e04479cbbeb119ba4
AndyDalaimo <adalaimo@gmail.com>
2024-08-18 17:39:07 +0100

    Grid popuated after settings path

--- Sprites/test_spritetscn.gd
+++ Sprites/test_spritetscn.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
-@export var _name = ""
+@export var _mesh_library_index : int = 0
 var local_pos
 var mouse_tile
 var move_this_piece = false
+var can_grab = true;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,18 +15,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var tempMouseTile = $"../../Map".local_to_map(get_global_mouse_position())
 	if (Input.is_action_pressed("MovePiece") && move_this_piece == true):
-		mouse_tile = BuildMap.tilemap.local_to_map(get_global_mouse_position())
-		local_pos = BuildMap.tilemap.map_to_local(mouse_tile)
-		if (BuildMap.tilemap.get_used_rect().has_point(mouse_tile) && BuildMap.tilemap.get_cell_source_id(mouse_tile) != 1):
+		mouse_tile = $"../../Map".local_to_map(get_global_mouse_position())
+		local_pos = $"../../Map".map_to_local(mouse_tile)
+		if ($"../../Map".get_used_rect().has_point(mouse_tile) && $"../../Map".get_cell_source_id(mouse_tile) == 0):
 			global_position = local_pos
-	elif (Input.is_action_just_released("MovePiece")):
+			can_grab = true;
+	elif (Input.is_action_just_released("MovePiece") && move_this_piece):
 		move_this_piece = false
+		$"../../Map".set_cell(tempMouseTile, -1)
+	if ($"../../Map".get_cell_source_id(tempMouseTile) != 0 && $"../../Map".get_used_rect().has_point(tempMouseTile)):
+		can_grab = false
+	else:
+		can_grab = true
+
 	#pass
 
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if get_rect().has_point(to_local(event.position)):
+		if get_rect().has_point(to_local(event.position)) && can_grab:
 			move_this_piece = true
 			
--- build_map.gd
+++ build_map.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-
-@onready var tilemap = get_node("Map")
+#@onready var tilemap = get_node("Map")
+@onready var simultaneous_scene = preload("res://GeneratedLevel/GeneratedLevel.tscn").instantiate()
+@export var MapGenResource : Resource
 var Sprites = []
 var BuildingCoords_Dict = {}
 
@@ -10,8 +11,7 @@ var BuildingCoords_Dict = {}
 func _ready():
 	for x in $Node2D.get_children():
 		Sprites.append(x)
-
-	#pass # Replace with function body.
+	pass # Replace with function body.
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,15 +19,28 @@ func _process(_delta):
 	pass
 
 func _GenerateMap():
-	BuildingCoords_Dict.clear()
+	MapGenResource.buildingcoords.clear()
 	for x in Sprites:
-		BuildingCoords_Dict.get_or_add(x.mouse_tile, x._name)
-	print(BuildingCoords_Dict)
+		MapGenResource.buildingcoords.get_or_add(x.mouse_tile, x._mesh_library_index)
+	#print(BuildingCoords_Dict)
 	
 func _GetBuildingCoords():
-	return BuildingCoords_Dict
+	return MapGenResource.buildingcoords
 	
 # DEBUG -- GenerateMap Function
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
 		_GenerateMap()
+		
+		
+func _on_button_pressed():
+	_GenerateMap()
+	get_tree().change_scene_to_file("res://GeneratedLevel/GeneratedLevel.tscn")
+	pass
+	
+	
+
+
+func _reload_button_pressed():
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
